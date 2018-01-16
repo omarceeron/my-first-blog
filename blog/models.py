6c04836c51ade00716aef50b51c5fa9c6dbeab74
omarceeron <omarceeron@gmail.com>
2018-01-16 13:02:58 -0600

    Mi app Django, primer commit

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+class Post(models.Model):
+	autor = models.CharField(max_length=200)
+	titulo = models.CharField(max_length=200)
+	texto = models.TextField()
+	fecha_de_creacion = models.DateTimeField(default=timezone.now)
+	fecha_de_publicacion = models.DateTimeField(blank=True, null=True)
+
+	def publicar(self):
+		self.fecha_de_publicacion = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.titulo
