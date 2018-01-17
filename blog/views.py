4ef140195610462698c11b0cc77c1bc6cfbdd7e5
omarceeron <omarceeron@gmail.com>
2018-01-17 16:20:38 -0600

    New design

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts =  Post.objects.filter(fecha_de_publicacion__lte=timezone.now()).order_by('fecha_de_publicacion')
+	return render(request, 'blog/post_list.html', {'posts': posts})
